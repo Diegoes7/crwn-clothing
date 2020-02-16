3c2d5dfd81dbfebb818bfb17f92eeacd1e2b1554
Diegoes7 <dimitar.i.dimitrov77@gmail.com>
2020-02-16 10:18:17 +0200

    Add checkout functionality and page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from 'react-redux'
-
+import { createStructuredSelector } from 'reselect'
+import { selectCurrentUser } from './redux/user/user.selectors'
+ 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepagecomponent";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from './pages/checkout/checkout.component'
+
+import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.util";
-import { setCurentUser } from './redux/user/user.actions'
+import { setCurrentUser } from './redux/user/user.actions'
 
 
 class App extends React.Component {
   unsubscribeFromAuth= null
 
   componentDidMount(){
-    const  { setCurentUser } = this.props
+    const  { setCurrentUser } = this.props
      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
 
         userRef.onSnapshot(snapShot => {
-         setCurentUser({
+         setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
           })
         })
       }
       else {
-        setCurentUser(userAuth )
+        setCurrentUser(userAuth )
       }
     })
   }
@@ -45,15 +49,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route exact path="/signin"
+           render={() => 
+           this.props.currentUser ?
+           (<Redirect to='/'/>) 
+          : (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  setCurentUser: user => dispatch(setCurentUser(user))
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
-export default connect(null,mapDispatchToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, 
+  mapDispatchToProps)
+  (App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import './header.styles.scss';
 import { Link } from "react-router-dom";
 import { auth }  from '../../firebase/firebase.util'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { connect } from 'react-redux'
+import { createStructuredSelector} from 'reselect'
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selectors'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -24,14 +29,19 @@ const Header = ({ currentUser }) => (
           SIGN OUT</div>)
         : (<Link className='option' to='/signin'>
           SIGN IN
-        </Link>)
-      }
+        </Link>
+        )}
+      <CartIcon />
     </div>
+    {
+      hidden ? null : <CartDropdown />
+    }
   </div>
 );
 
-const mapStateToProps = state =>({
-  currentUser: state.user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header)
